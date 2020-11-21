90a2012639482022970890b9f034ae5e2a16667d
yoga-13 <40943132@gm.nfu.edu.tw>
2020-11-21 19:39:47 +0800

    Change my client_id

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const { router, text, line, messenger } = require('bottender/router');
 const handler = require('./handler')
-
+const 排名 = require('./排名')
 exports.App = () => {
     return router([
         text(/^(hi|hello|help)$/i, handler.HandleFollow),
+        text(/^(排名)$/i, 排名.HandleFollow),
 
         line.message(handler.HandleLineMessage),
         line.follow(handler.HandleFollow),
