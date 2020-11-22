e3a1153544fb16ebc8b7c0617d55e64f41fb4a06
yoga-13 <40943132@gm.nfu.edu.tw>
2020-11-22 15:11:32 +0800

    new

--- src/handler.js
+++ src/handler.js
@@ -7,11 +7,11 @@ const welcomeMessage = 'Hi~ 本 Bot 是用 https://rebrand.ly/ic-chatbot-github 
     '音樂：「播放告白氣球」；「播放自傳專輯的歌」；「我要聽鄉村音樂」；「我要聽日文新歌」\n' + 
     '音樂活動：「查詢高雄的活動」；「查詢吳卓源的演場會」；「查詢兩廳院的表演」\n' +
     '影音內容：「查詢影片進擊的巨人」；「查詢日劇半澤直樹」\n' +
-    '輸入help顯示幫助';
+    '輸入help顯示功能';
 const RankMessage = 'https://kma.kkbox.com/charts/?terr=tw&lang=tc ';
 const HelpMessage = '功能\n'+
                     '輸入help顯示功能\n'+
-                    '輸入rank顯示排名\n';
+                    '輸入rank顯示排名';
 
 exports.HandleLineMessage = async context => {
     if (context.event.isText) {
@@ -49,6 +49,30 @@ exports.help = async context => {
 }
 
 
+exports.testHandleLineMessage = async context => {
+    if (context.event.isText) {
+        kkassistant.nlu(context.event.text, context.session.id)
+            .then(nluResp => {
+                if (nluResp.directives.length > 0) {
+                    if(nluResp.directives[0].type == 'AudioPlayer.Play') {
+                        return kkbox.fetchTracks(nluResp.directives[0].playlist.data);
+                    } else { // Event.Metadata & Video.Metadata
+                        return nluResp.directives[0];
+                    }
+                }
+                else {
+                    console.error('Error: ', nluResp);
+                    context.sendText(nluResp.outputSpeech.text);
+                    throw new Error('KKBOX Assistant NLP Error');
+                }
+            })
+            .then(items => new KKBOXMessage(items).toLineMessage())
+            .then(({ altText, template }) => context.sendImageCarouselTemplate(altText, template))
+            .catch(error => {
+                console.error('Error: ', error);
+            });
+    }
+}
 
 
 
